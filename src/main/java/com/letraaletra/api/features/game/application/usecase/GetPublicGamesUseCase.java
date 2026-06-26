b36c4ba8779f9fa02e049d16e40b9e2d61d05dec
Zidan-09 <samuel.profissional18@gmail.com>
2026-06-26 19:37:08 -0300

    refactor: start to remove other UseCase interface uses

--- src/main/java/com/letraaletra/api/features/game/application/usecase/GetPublicGamesUseCase.java
+++ src/main/java/com/letraaletra/api/features/game/application/usecase/GetPublicGamesUseCase.java
@@ -2,19 +2,19 @@ package com.letraaletra.api.features.game.application.usecase;
 
 import com.letraaletra.api.features.game.application.output.GetGamesOutput;
 import com.letraaletra.api.features.game.application.port.GameQueryService;
-import com.letraaletra.api.shared.application.usecase.UseCaseWithoutInput;
+import com.letraaletra.api.shared.application.usecase.UseCase;
 import com.letraaletra.api.features.game.domain.Game;
 
 import java.util.List;
 
-public class GetPublicGamesUseCase implements UseCaseWithoutInput<GetGamesOutput> {
+public class GetPublicGamesUseCase implements UseCase<Void, GetGamesOutput> {
     private final GameQueryService gameQueryService;
 
     public GetPublicGamesUseCase(GameQueryService gameQueryService) {
         this.gameQueryService = gameQueryService;
     }
 
-    public GetGamesOutput execute() {
+    public GetGamesOutput execute(Void input) {
         List<Game> games = gameQueryService.getPublic();
 
         return buildOutput(games);
