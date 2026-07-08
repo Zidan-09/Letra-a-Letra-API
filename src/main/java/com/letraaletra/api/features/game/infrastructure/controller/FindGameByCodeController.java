29179d94181fa9bf36c0fb40c84fc748ad9720fd
Zidan-09 <samuel.profissional18@gmail.com>
2026-07-08 14:29:35 -0300

    refactor: refactored controllers to have dependence inversion

--- src/main/java/com/letraaletra/api/features/game/infrastructure/controller/FindGameByCodeController.java
+++ src/main/java/com/letraaletra/api/features/game/infrastructure/controller/FindGameByCodeController.java
@@ -2,10 +2,10 @@ package com.letraaletra.api.features.game.infrastructure.controller;
 
 import com.letraaletra.api.features.game.application.input.FindByCodeInput;
 import com.letraaletra.api.features.game.application.output.FindByCodeOutput;
-import com.letraaletra.api.features.game.application.usecase.FindByCodeUseCase;
 import com.letraaletra.api.features.game.infrastructure.presentation.mapper.game.FindByCodeMapper;
 import com.letraaletra.api.shared.application.service.ApiResponseService;
 import com.letraaletra.api.features.game.infrastructure.presentation.dto.response.FindByCodeResponse;
+import com.letraaletra.api.shared.application.usecase.UseCase;
 import com.letraaletra.api.shared.infrastructure.presentation.dto.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
@@ -19,9 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/game")
 @Tag(name = "Game", description = "Rotas relacionadas a funcionalidade de salas")
 public class FindGameByCodeController {
-    private final FindByCodeUseCase findByCodeUseCase;
+    private final UseCase<FindByCodeInput, FindByCodeOutput> findByCodeUseCase;
 
-    public FindGameByCodeController(FindByCodeUseCase findByCodeUseCase) {
+    public FindGameByCodeController(UseCase<FindByCodeInput, FindByCodeOutput> findByCodeUseCase) {
         this.findByCodeUseCase = findByCodeUseCase;
     }
 
