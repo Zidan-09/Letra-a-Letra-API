9b7de32fb1cfa7df0a9ef4bcac83624f2fef894b
Zidan-09 <samuel.profissional18@gmail.com>
2026-07-10 17:52:23 -0300

    refactor: used validation on dtos

--- src/main/java/com/letraaletra/api/features/offers/infrastructure/presentation/dto/request/RegisterOfferRequest.java
+++ src/main/java/com/letraaletra/api/features/offers/infrastructure/presentation/dto/request/RegisterOfferRequest.java
@@ -1,14 +1,26 @@
 package com.letraaletra.api.features.offers.infrastructure.presentation.dto.request;
 
 import com.letraaletra.api.features.offers.domain.CoinType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record RegisterOfferRequest(
+        @NotBlank
         String title,
+
+        @NotBlank
         CoinType coinType,
+
+        @NotNull
         int price,
+
+        @Valid
         List<RegisterOfferRewardRequest> rewards,
+
+        @NotNull
         long expiresIn
 ) {
 }
