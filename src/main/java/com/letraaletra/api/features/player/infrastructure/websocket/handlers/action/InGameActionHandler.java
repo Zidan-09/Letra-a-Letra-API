83b84c7b09729b65b00db500b12d1b2c01d7a0e8
Zidan-09 <samuel.profissional18@gmail.com>
2026-07-24 20:21:48 -0300

    feat: implemented game logs per match, date and game

--- src/main/java/com/letraaletra/api/features/player/infrastructure/websocket/handlers/action/InGameActionHandler.java
+++ src/main/java/com/letraaletra/api/features/player/infrastructure/websocket/handlers/action/InGameActionHandler.java
@@ -1,10 +1,11 @@
 package com.letraaletra.api.features.player.infrastructure.websocket.handlers.action;
 
+import com.letraaletra.api.features.game.domain.Game;
 import com.letraaletra.api.features.player.infrastructure.presentation.dto.request.PlayerActionRequest;
 import org.springframework.web.socket.WebSocketSession;
 
 public interface InGameActionHandler<T extends PlayerActionRequest> {
-    void handle(T request, WebSocketSession session, String gameId);
+    Game handle(T request, WebSocketSession session, String gameId);
 
     Class<T> getType();
 }
