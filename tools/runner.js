c1df0e717bd728e5119a920e0eb89156dafc905f
Zidan-09 <samuel.profissional18@gmail.com>
2026-09-10 16:56:58 -0300

    fix: deleted E2E tests for shop because H2 dont have Storaged Procedures

--- tools/runner.js
+++ tools/runner.js
@@ -13,7 +13,6 @@ import { run as gameLobby } from "./tests/game-lobby.test.js";
 import { run as matchmakingExit } from "./tests/matchmaking-exit.test.js";
 import { run as rankingExit } from "./tests/ranking-exit.test.js";
 import { run as gameMatch } from "./tests/game-match.test.js";
-import { run as shop } from "./tests/shop.test.js";
 
 const tests = [
     adminRoutes,
